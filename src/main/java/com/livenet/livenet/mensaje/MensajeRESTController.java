0c8559af2fd421fb474c9df6d068cdfd7fee5be6
Emilio Rubiales <vintajeskull98@gmail.com>
2020-01-25 14:01:43 +0100

    Mensajes, Sesiones

--- src/main/java/com/livenet/livenet/mensaje/MensajeRESTController.java
+++ src/main/java/com/livenet/livenet/mensaje/MensajeRESTController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/mensajes")
+@RequestMapping("/")
 public class MensajeRESTController {
 
 
