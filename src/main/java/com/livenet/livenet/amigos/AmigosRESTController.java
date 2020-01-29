0d74741834bd0f69d8ce20dd6fe9af5ae5d4087d
Emilio Rubiales <vintajeskull98@gmail.com>
2020-01-30 00:56:38 +0100

    Sistema de amigos

--- src/main/java/com/livenet/livenet/amigos/AmigosRESTController.java
+++ src/main/java/com/livenet/livenet/amigos/AmigosRESTController.java
@@ -5,11 +5,9 @@ import com.livenet.livenet.usuario.Usuario;
 import com.livenet.livenet.usuario.usuariosDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.Response;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,6 +40,19 @@ public class AmigosRESTController {
         }
 
         return ResponseEntity.ok(users);
+    }
+
+    @RequestMapping(value="agregaramigo", method = RequestMethod.POST)
+    public ResponseEntity<String> create(@RequestBody String[] amigo){
+        Amigo resultado = pd.findByAlias1AndAlias2(amigo[0], amigo[1]);
+        Amigo resultado2 = pd.findByAlias1AndAlias2(amigo[1], amigo[0]);
+        if(resultado != null || resultado2 != null){
+            return ResponseEntity.noContent().build();
+        }else{
+            pd.save(new Amigo(amigo[0], amigo[1]));
+
+        }
+        return ResponseEntity.ok("ok google");
     }
 
 }
